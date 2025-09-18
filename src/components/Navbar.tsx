8e747ec430311cfddc87b09a55add471e7cba473
Varun Mahagaokar <varun090295@gmail.com>
2025-09-17 21:22:19 -0400

    remove unwanted images

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 // src/components/Navbar.jsx → RENAME to Navbar.tsx if you're using TypeScript
+
+import { Nav } from "react-bootstrap";
 import styles from "./Navbar.module.scss"; // ✅ Import SCSS Module
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -22,25 +25,29 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className={`navbar-nav ms-auto ${styles.navbarNav}`}>
+            <Nav.Link
+              as={NavLink}
+              to="/"
+              // @ts-ignore
+              className={({ isActive, isPending }) => {
+                console.log("hello");
+                isPending ? "pending" : isActive ? "active" : "";
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/menu">
+              Menu
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/about">
+              About us
+            </Nav.Link>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
+              <a href="https://www.google.com/viewer/chooseprovider?mid=/g/11q33zvkz5&g2lbs=AO8LyOI65lpBhTXWeYuolbKx3qz1LVN5TrmVLSybnA12n6Bhd22EETdrmnqPTvGQH2U1GS4zIgSlpwsumCAynLXSohkQAVsBIA%3D%3D&hl=en-CA&gl=ca&ssta=1&fo_m=MfohQo559jFvMUOzJVpjPL1YMfZ3bInYwBDuMfaXTPp5KXh-&utm_source=search&gei=AF3LaOjJHrDn5NoPwqaosA4&ei=AF3LaOjJHrDn5NoPwqaosA4&fo_s=OA&opi=89978449&ebb=1&foub=mcpp&orderType=2">
+                <button type="button" className={`btn-success btn btn-sm`}>
+                  Order Online
+                </button>
               </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Menu
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <button type="button" className={`${styles.orderBtn} btn btn-sm`}>
-                Order Online
-              </button>
             </li>
           </ul>
         </div>
