ec9572277a3f172fe683fa10836b54395912e951
Varun Mahagaokar <varun090295@gmail.com>
2025-09-18 23:23:19 -0400

    resolved routing issue.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -11,7 +11,7 @@ import Banner from "./components/Banner.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       {/* <OfferBanner
         message="20% OFF all order this weekend. Use code WALL20 "
         pillText="Grand Opening"
@@ -20,6 +20,6 @@ createRoot(document.getElementById("root")!).render(
       <Navbar />
       <Banner position="bottom" />
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
